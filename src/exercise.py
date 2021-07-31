24f0165f8e215b3e5d1395ae55613ff37fcc6bcf
Justin Turner <jmturner638@gmail.com>
2021-07-31 00:32:31 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    print(average(4,3,6,1))
+
+def sum(number1, number2, number3, number4):
+    return number1 + number2 + number3 + number4
+
+def average(number1, number2, number3, number4):
+    return sum(number1, number2, number3, number4) / 4
 
 if __name__ == '__main__':
     main()
